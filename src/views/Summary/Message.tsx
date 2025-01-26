65bd9253899b7dc728b9d383fbe6e01236da5e97
loktar00 <loktar00@gmail.com>
2025-01-25 21:45:19 -0500

    Added copying to clipboard for all messages

--- src/views/Summary/Message.tsx
+++ src/views/Summary/Message.tsx
@@ -1,9 +1,23 @@
 import { memo } from 'react';
+import { IoCopyOutline } from "react-icons/io5";
 import { markdownToHtml } from '@/utils/markdown';
+import { toast } from 'react-toastify';
 import { Message as MessageType } from './types';
+
+const handleCopyToClipboard = async (content: string) => {
+    try {
+        await navigator.clipboard.writeText(content);
+        toast.success("Transcript copied to clipboard!");
+    } catch (error) {
+        toast.error(`Failed to copy text to clipboard. ${error instanceof Error ? error.message : 'Unknown error'}`);
+    }
+}
 
 export const Message = memo(({ role, content }: MessageType) => (
     <div className={`message ${role}-message`}>
         <div className="markdown-body" dangerouslySetInnerHTML={{ __html: markdownToHtml(content) }} />
+        <div className="message-controls">
+            <IoCopyOutline onClick={() => handleCopyToClipboard(content)} size={20}/>
+        </div>
     </div>
 ));
